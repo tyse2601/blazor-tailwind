ed98f9aa8f7d1b89a8889de6554802660d210b53
Black Panther <andrew.tyson2601@gmail.com>
2021-06-30 21:45:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './wwwroot/index.html',
+        './**/*.razor',
+    ],
+    darkMode: 'media',
+    theme: {
+        extend: {
+            colors: {
+                cyan: colors.cyan
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('tailwindcss-textshadow')
+    ]
+}
