bf34692a91c62ddb36ad1c8a71bc23377df00ac3
tyse2601 <andrew.tyson2601@gmail.com>
2021-09-30 09:47:38 +0100

    change

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+// using BlazorInAction.Data;
 
 namespace BlazorInAction
 {
